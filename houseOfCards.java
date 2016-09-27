43fbb7c1814286059fa127b306e7a266d4ef74e8
F4llenPotAto <t.man.rockz@gmail.com>
2016-09-27 10:34:49 -0500

    Update and rename README.md to houseOfCards.java

---
+++ houseOfCards.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+import static java.lang.System.*;
+
+public class houseOfCards {
+  public static void main(String[] args) throws IOException{
+       Scanner sc = new Scanner(new File("cards.txt"));
+       int deckNumb = sc.nextInt();
+       
+       
+
+  }
+}
