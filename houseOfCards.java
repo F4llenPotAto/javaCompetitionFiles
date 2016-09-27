55a50144ab988dfd8ac5e8b9ccadc82599da9909
F4llenPotAto <t.man.rockz@gmail.com>
2016-09-27 11:28:56 -0500

    Update houseOfCards.java

--- houseOfCards.java
+++ houseOfCards.java
@@ -4,20 +4,21 @@ import static java.lang.System.*;
 
 public class houseOfCards {
   public static void main(String[] args) throws IOException{
-    Scanner sc = new Scanner(new File("cards.txt"));
+    Scanner sc = new Scanner(new File("cards.dat"));
     int deckNumb = sc.nextInt();
     int cases = sc.nextInt();
-    int totalNumCards = deckNumbs * 52;
-    int cardsNeeded = 0;
-    int numberOfTowers = 0;
-    
+    int totalNumCards = deckNumb * 52;
+
     for(int i = 0; i < cases; i ++){
-      int height = sc.nextInt();
-      for(int j =0; j < height; j ++){
-        cardsNeeded+= ((3*j)-1);    
-      }
-      numberOfTowers = cardsNeeded/deckNumb;
-      out.println(numberOfTowers);
-    }  
+    	int numberOfTowers = 0;
+    	int cardsNeeded = 0;
+    	int height = sc.nextInt();
+    		for(int j =0; j < height; j ++){
+    			cardsNeeded+=((3*j)-1);
+    		}
+    numberOfTowers = totalNumCards/cardsNeeded;
+	out.println(numberOfTowers);
+
+    }
   }
 }
